802278f82d6c85a39c0b680cc021f407cd6286df
SABBIR-PC <SABBIR-PC@SABBIR-PC>
2016-09-28 23:36:09 +0600

    Add project files.

---
+++ src/TourManager/Controllers/Web/AppController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TourManager.Services;
+using TourManager.ViewModels;
+
+namespace TourManager.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _config = config;
+            _mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We don't Support AOL address");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From Tour Manager", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
+
+            return View();
+
+
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
